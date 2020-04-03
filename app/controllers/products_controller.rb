c0d2c3c037f6d52bc4a1d7f448d9636f771be14f
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-04-03 17:05:34 +0900

    Add update method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product, except: [:index, :new, :create]
+
   def index
     @products = Product.includes(:images).order('created_at DESC')
   end
@@ -21,6 +23,11 @@ class ProductsController < ApplicationController
   end
 
   def update
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -29,6 +36,10 @@ class ProductsController < ApplicationController
   private
 
     def product_params
-      params.require(:product).permit(:name, :price, images_attributes: [:src])
+      params.require(:product).permit(:name, :price, images_attributes: [:src, :_destroy, :id])
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
     end
 end
